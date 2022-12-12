5dfd53488ba114abcf06898b70d2e785b6b8be60
Kent 'picat' Gruber <kent.picat.gruber@gmail.com>
2022-12-12 03:57:37 -0500

    Add diagram

--- pkg/websockets/doc.go
+++ pkg/websockets/doc.go
@@ -4,4 +4,22 @@
 //
 // This is useful for applications that want to have a two-way
 // communication channel with the browser.
+//
+// Diagram
+//
+//	+----------------+                          +----------------+
+//	|     Client     |                          |     Server     |
+//	+----------------+                          +----------------+
+//			|                                           |
+//			|------------ GET /chat HTTP/1.1 ---------->|
+//			|                                           |
+//			|<- - HTTP/1.1 101 Switching Protocols - - -|
+//			|                                           |
+//			|<---------~ WebSockets Handshake ~-------->|
+//			|                                           |
+//			|------ Frame: TextMessage, "Hello" ------->|
+//			|                                           |
+//			|<------ Frame: TextMessage, "Hello" -------|
+//			|                                           |
+//			.                                           .
 package websockets
